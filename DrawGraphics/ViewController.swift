c70cc7f32e9d9584de758a8decb8d6f64e77e17e
권민수 <kd0615@naver.com>
2023-05-26 12:16:13 +0900

    그림미션 완성

--- DrawGraphics/ViewController.swift
+++ DrawGraphics/ViewController.swift
@@ -121,5 +121,47 @@ class ViewController: UIViewController {
         UIGraphicsEndImageContext()
     }
     
+    @IBAction func btnDrawCustom(_ sender: UIButton) {
+        UIGraphicsBeginImageContext(imgView.frame.size)
+        let context = UIGraphicsGetCurrentContext()!
+        
+        context.setLineWidth(1.0)
+        context.setStrokeColor(UIColor.green.cgColor)
+        context.setFillColor(UIColor.green.cgColor)
+        context.move(to: CGPoint(x: 170, y: 200))
+        context.addLine(to: CGPoint(x: 200, y: 450))
+        context.addLine(to: CGPoint(x: 140, y: 450))
+        context.addLine(to: CGPoint(x: 170, y: 200))
+        context.fillPath()
+        context.strokePath()
+        
+        context.setLineWidth(2.0)
+        context.setStrokeColor(UIColor.magenta.cgColor)
+        context.addEllipse(in: CGRect(x: 120, y: 150, width: 100, height: 100))
+        context.strokePath()
+        
+        context.setLineWidth(2.0)
+        context.setStrokeColor(UIColor.magenta.cgColor)
+        context.addEllipse(in: CGRect(x: 170, y: 150, width: 100, height: 100))
+        context.strokePath()
+        
+        context.setLineWidth(2.0)
+        context.setStrokeColor(UIColor.magenta.cgColor)
+        context.addEllipse(in: CGRect(x: 70, y: 150, width: 100, height: 100))
+        context.strokePath()
+        
+        context.setLineWidth(2.0)
+        context.setStrokeColor(UIColor.magenta.cgColor)
+        context.addEllipse(in: CGRect(x: 120, y: 100, width: 100, height: 100))
+        context.strokePath()
+        
+        context.setLineWidth(2.0)
+        context.setStrokeColor(UIColor.magenta.cgColor)
+        context.addEllipse(in: CGRect(x: 120, y: 200, width: 100, height: 100))
+        context.strokePath()
+        
+        imgView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
 }
 
